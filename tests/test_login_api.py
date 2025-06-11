f6fc2ce3ef5507751198552dad8b74b8e9696149
iskvoznyakov <iliaskvozniakov@mail.ru>
2025-06-11 22:26:08 +0400

    feat: integrate Allure reporting for test cases - Installed allure-pytest and added to requirements.txt - Configured pytest.ini to save results to allure-results directory - Added Allure annotations - Updated existing login tests with Allure reporting structure

--- tests/test_login_api.py
+++ tests/test_login_api.py
@@ -1,13 +1,28 @@
 from models import LoginResponse
+import allure
 
 
+@allure.feature("Login")
+@allure.story("Successful login")
+@allure.title("Successful login with valid email and password")
+@allure.description("Тест проверяет успешный логин с валидными email и password.")
 def test_successful_login(client):
-    response = client.login(email="eve.holt@reqres.in", password="cityslicka")
-    assert isinstance(response, LoginResponse)
-    assert response.token
+    with allure.step("Выполняем запрос логина с валидными данными"):
+        response = client.login(email="eve.holt@reqres.in", password="cityslicka")
+    with allure.step("Проверяем, что ответ является экземпляром LoginResponse"):
+        assert isinstance(response, LoginResponse)
+    with allure.step("Проверяем, что в ответе есть токен"):
+        assert response.token
 
 
+@allure.feature("Login")
+@allure.story("Failed login")
+@allure.title("Unsuccessful login with valid email and empty password")
+@allure.description("Тест проверяет ошибку при попытке логина без пароля.")
 def test_login_missing_password(client):
-    response = client.login(email="eve.holt@reqres.in", password="", raise_on_error=False)
-    assert response.status_code == 400, f"При попытке логина без пароля - вернулся {response.status_code} статус-код"
-    assert response.json()["error"] == "Missing password"
+    with allure.step("Выполняем запрос логина без пароля"):
+        response = client.login(email="eve.holt@reqres.in", password="", raise_on_error=False)
+    with allure.step("Проверяем, что статус код 400"):
+        assert response.status_code == 400, f"При попытке логина без пароля - вернулся {response.status_code} статус-код"
+    with allure.step("Проверяем, что сообщение об ошибке 'Missing password' присутствует в ответе"):
+        assert response.json()["error"] == "Missing password"
