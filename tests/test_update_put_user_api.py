e2283ed3f19dc2d85c775e8e0de2306574f12e7a
Ilya Skvoznyakov <104623164+iskvoznyakov@users.noreply.github.com>
2025-06-18 14:06:13 +0400

    feat(tests): add negative test for updating user via PUT (#10)

--- tests/test_update_put_user_api.py
+++ tests/test_update_put_user_api.py
@@ -24,3 +24,21 @@ def test_successful_update_user_by_id_with_put(client, user_id, name, job):
         assert min_expected <= response.updatedAt <= max_expected, (
             f"Ожидали время между {min_expected} и {max_expected}, но получили {response.updatedAt}"
         )
+
+
+@pytest.mark.parametrize("user_id, name, job",
+                         [(-1, "Ivan", "QA Engineer"),
+                          (2, '', ''),
+                          (1, 2, 3),
+                          (4, '!@#$%^&*()', '!@#$%^&*()')])
+@pytest.mark.xfail(reason="API incorrectly accepts invalid update payloads and returns success.")
+@allure.feature("Updating user")
+@allure.story("Updating user with invalid data")
+@allure.title("User update should fail with invalid data: {user_id, name, job}")
+@allure.description(
+    "Нежелательное поведение: API принимает невалидные данные и возвращает успешный ответ вместо ошибки.")
+def test_failed_update_user_by_id_with_put(client, user_id, name, job):
+    with allure.step("Выполняем запрос обновления пользователя с невалидными данными"):
+        response = client.update_user_by_id_with_put(user_id=user_id, name=name, job=job, raise_on_error=False)
+    with allure.step("Проверяем статус-код ответа"):
+        assert response.status_code == 400, f"Ожидали 400 статус-код, получили {response.status_code}"
