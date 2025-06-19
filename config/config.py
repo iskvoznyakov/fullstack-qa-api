49784948e6e286e35fa2ee567846295ef2c751f4
Ilya Skvoznyakov <104623164+iskvoznyakov@users.noreply.github.com>
2025-06-19 08:22:22 +0400

    feat(config): move base URL and API key to .env using python-dotenv (#11)
    * feat(config): move base URL and API key to .env using python-dotenv
    
    * feat: add support for API_KEY via environment variables and GitHub Secrets
    
    * feat: a little refactoring to BASE_URL in config.py

--- config/config.py
+++ config/config.py
@@ -1,2 +1,10 @@
-BASE_URL = "https://reqres.in/api"
-API_KEY = "reqres-free-v1"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_URL = os.getenv("BASE_URL", "https://reqres.in/api")
+
+API_KEY = os.getenv("API_KEY")
+if not API_KEY:
+    raise ValueError("API_KEY is not set in .env")
